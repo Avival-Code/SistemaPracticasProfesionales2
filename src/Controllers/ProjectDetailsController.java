95dab0b3b36715a1a518189b0750b124b0586b8b
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-04-29 20:31:56 -0500

    Implemented SetStudentNames and GetStudentNames in ProjectDetailsController class.

--- src/Controllers/ProjectDetailsController.java
+++ src/Controllers/ProjectDetailsController.java
@@ -1,10 +1,7 @@
 package Controllers;
 
 import Database.*;
-import Entities.Expediente;
-import Entities.OrganizacionVinculada;
-import Entities.Proyecto;
-import Entities.ResponsableProyecto;
+import Entities.*;
 import Enumerations.EstadoProyecto;
 import Utilities.ScreenChanger;
 import javafx.fxml.FXML;
@@ -14,6 +11,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import sample.LoginSession;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -21,6 +19,7 @@ public class ProjectDetailsController implements Initializable {
     private ScreenChanger screenChanger = new ScreenChanger();
     private ExpedienteDAO expedientes = new ExpedienteDAO();
     private ProyectoDAO proyectos = new ProyectoDAO();
+    private EstudianteDAO estudiantes = new EstudianteDAO();
     private OrganizacionVinculadaDAO organizaciones = new OrganizacionVinculadaDAO();
     private ResponsableProyectoDAO responsables = new ResponsableProyectoDAO();
     private ResponsablesOrganizacionDAO responsablesOrganizacion = new ResponsablesOrganizacionDAO();
@@ -81,11 +80,12 @@ public class ProjectDetailsController implements Initializable {
     private Text telefonoText;
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize( URL url, ResourceBundle resourceBundle ) {
         SetUserInformation();
         SetProjectTexts();
         SetResponsableTexts();
         SetOrganizacionTexts();
+        SetStudentNames( GetStudentNames() );
     }
 
     private void SetUserInformation() {
@@ -117,6 +117,44 @@ public class ProjectDetailsController implements Initializable {
         organizacion = organizaciones.Read( responsablesOrganizacion.ReadOrganizacion( responsable.getIdResponsableProyecto() ) );
         organizacionText.setText( organizacion.getNombre() );
         direccionText.setText( organizacion.getDireccion() );
+    }
+
+    private void SetStudentNames( List< String > students ) {
+        switch( students.size() ) {
+            case 1:
+                studentName1.setText( students.get( 0 ) );
+                break;
+
+            case 2:
+                studentName1.setText( students.get( 0 ) );
+                studentName2.setText( students.get( 1 ) );
+                break;
+
+            case 3:
+                studentName1.setText( students.get( 0 ) );
+                studentName2.setText( students.get( 1 ) );
+                studentName3.setText( students.get( 2 ) );
+                break;
+
+            case 4:
+                studentName1.setText( students.get( 0 ) );
+                studentName2.setText( students.get( 1 ) );
+                studentName3.setText( students.get( 2 ) );
+                studentName4.setText( students.get( 3 ) );
+                break;
+        }
+    }
+
+    private List< String > GetStudentNames() {
+        List< String > studentNames = new ArrayList<>();
+        for( Expediente expedienteEstudiante : expedientes.ReadAll() )
+        {
+            if( expedienteEstudiante.GetIDProyecto() == proyecto.getIdProyecto() ) {
+                studentNames.add( estudiantes.Read( expedienteEstudiante.GetMatricula() ).getNombres() + " " +
+                                  estudiantes.Read( expedienteEstudiante.GetMatricula() ).GetApellidos() );
+            }
+        }
+        return studentNames;
     }
 
     private Expediente GetUserExpediente() {
